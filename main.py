5f3890447aad65ad46e3f2468df685f401e70a77
Federico <federico@indently.io>
2024-07-23 08:47:11 +0200

    added this

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-# This is a sample Python script.
-
-# Press ⌘R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name) -> None:
-    print(f'Hi, {name}')
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import this
